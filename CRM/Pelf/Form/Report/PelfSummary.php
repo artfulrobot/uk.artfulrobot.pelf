a4202bd4d92bd87fc0f463ca8633144b4292c457
Rich Lott <forums@artfulrobot.uk>
2016-03-21 13:49:21 +0000

    Initial import

---
+++ CRM/Pelf/Form/Report/PelfSummary.php
@@ -0,0 +1,469 @@
+<?php
+use \ArtfulRobot as ARL;
+use \ArtfulRobot\CiviCRM as ARLCRM;
+
+class CRM_Pelf_Form_Report_PelfSummary extends CRM_Report_Form {
+
+  protected $_addressField = FALSE;
+
+  protected $_emailField = FALSE;
+
+  protected $_summary = NULL;
+
+  protected $_customGroupExtends = array();
+  protected $_customGroupGroupBy = FALSE;
+  function __construct() {
+    // This is not needed; there are no options.
+    if (FALSE) {
+      $this->_columns = array(
+      'civicrm_contact' => array(
+        'dao' => 'CRM_Contact_DAO_Contact',
+        'fields' => array(
+          'sort_name' => array(
+            'title' => ts('Contact Name'),
+            'required' => TRUE,
+            'default' => TRUE,
+            'no_repeat' => TRUE,
+          ),
+          'id' => array(
+            'no_display' => TRUE,
+            'required' => TRUE,
+          ),
+          'first_name' => array(
+            'title' => ts('First Name'),
+            'no_repeat' => TRUE,
+          ),
+          'id' => array(
+            'no_display' => TRUE,
+            'required' => TRUE,
+          ),
+          'last_name' => array(
+            'title' => ts('Last Name'),
+            'no_repeat' => TRUE,
+          ),
+          'id' => array(
+            'no_display' => TRUE,
+            'required' => TRUE,
+          ),
+        ),
+        'filters' => array(
+          'sort_name' => array(
+            'title' => ts('Contact Name'),
+            'operator' => 'like',
+          ),
+          'id' => array(
+            'no_display' => TRUE,
+          ),
+        ),
+        'grouping' => 'contact-fields',
+      ),
+      'civicrm_membership' => array(
+        'dao' => 'CRM_Member_DAO_Membership',
+        'fields' => array(
+          'membership_type_id' => array(
+            'title' => 'Membership Type',
+            'required' => TRUE,
+            'no_repeat' => TRUE,
+          ),
+          'join_date' => array('title' => ts('Join Date'),
+            'default' => TRUE,
+          ),
+          'source' => array('title' => 'Source'),
+        ),
+        'filters' => array(
+          'join_date' => array(
+            'operatorType' => CRM_Report_Form::OP_DATE,
+          ),
+          'owner_membership_id' => array(
+            'title' => ts('Membership Owner ID'),
+            'operatorType' => CRM_Report_Form::OP_INT,
+          ),
+          'tid' => array(
+            'name' => 'membership_type_id',
+            'title' => ts('Membership Types'),
+            'type' => CRM_Utils_Type::T_INT,
+            'operatorType' => CRM_Report_Form::OP_MULTISELECT,
+            'options' => CRM_Member_PseudoConstant::membershipType(),
+          ),
+        ),
+        'grouping' => 'member-fields',
+      ),
+      'civicrm_membership_status' => array(
+        'dao' => 'CRM_Member_DAO_MembershipStatus',
+        'alias' => 'mem_status',
+        'fields' => array(
+          'name' => array(
+            'title' => ts('Status'),
+            'default' => TRUE,
+          ),
+        ),
+        'filters' => array(
+          'sid' => array(
+            'name' => 'id',
+            'title' => ts('Status'),
+            'type' => CRM_Utils_Type::T_INT,
+            'operatorType' => CRM_Report_Form::OP_MULTISELECT,
+            'options' => CRM_Member_PseudoConstant::membershipStatus(NULL, NULL, 'label'),
+          ),
+        ),
+        'grouping' => 'member-fields',
+      ),
+      'civicrm_address' => array(
+        'dao' => 'CRM_Core_DAO_Address',
+        'fields' => array(
+          'street_address' => NULL,
+          'city' => NULL,
+          'postal_code' => NULL,
+          'state_province_id' => array('title' => ts('State/Province')),
+          'country_id' => array('title' => ts('Country')),
+        ),
+        'grouping' => 'contact-fields',
+      ),
+      'civicrm_email' => array(
+        'dao' => 'CRM_Core_DAO_Email',
+        'fields' => array('email' => NULL),
+        'grouping' => 'contact-fields',
+      ),
+    );
+    }
+    //$this->_groupFilter = TRUE;
+    //$this->_tagFilter = TRUE;
+    parent::__construct();
+  }
+
+  function preProcess() {
+    $this->assign('reportTitle', ts('Membership Detail Report'));
+    parent::preProcess();
+  }
+
+  function select() {
+    $select = $this->_columnHeaders = array();
+
+    foreach ($this->_columns as $tableName => $table) {
+      if (array_key_exists('fields', $table)) {
+        foreach ($table['fields'] as $fieldName => $field) {
+          if (CRM_Utils_Array::value('required', $field) ||
+            CRM_Utils_Array::value($fieldName, $this->_params['fields'])
+          ) {
+            if ($tableName == 'civicrm_address') {
+              $this->_addressField = TRUE;
+            }
+            elseif ($tableName == 'civicrm_email') {
+              $this->_emailField = TRUE;
+            }
+            $select[] = "{$field['dbAlias']} as {$tableName}_{$fieldName}";
+            $this->_columnHeaders["{$tableName}_{$fieldName}"]['title'] = $field['title'];
+            $this->_columnHeaders["{$tableName}_{$fieldName}"]['type'] = CRM_Utils_Array::value('type', $field);
+          }
+        }
+      }
+    }
+
+    $this->_select = "SELECT " . implode(', ', $select) . " ";
+  }
+
+  function from() {
+    $this->_from = NULL;
+
+    $this->_from = "
+         FROM  civicrm_contact {$this->_aliases['civicrm_contact']} {$this->_aclFrom}
+               INNER JOIN civicrm_membership {$this->_aliases['civicrm_membership']}
+                          ON {$this->_aliases['civicrm_contact']}.id =
+                             {$this->_aliases['civicrm_membership']}.contact_id AND {$this->_aliases['civicrm_membership']}.is_test = 0
+               LEFT  JOIN civicrm_membership_status {$this->_aliases['civicrm_membership_status']}
+                          ON {$this->_aliases['civicrm_membership_status']}.id =
+                             {$this->_aliases['civicrm_membership']}.status_id ";
+
+
+    //used when address field is selected
+    if ($this->_addressField) {
+      $this->_from .= "
+             LEFT JOIN civicrm_address {$this->_aliases['civicrm_address']}
+                       ON {$this->_aliases['civicrm_contact']}.id =
+                          {$this->_aliases['civicrm_address']}.contact_id AND
+                          {$this->_aliases['civicrm_address']}.is_primary = 1\n";
+    }
+    //used when email field is selected
+    if ($this->_emailField) {
+      $this->_from .= "
+              LEFT JOIN civicrm_email {$this->_aliases['civicrm_email']}
+                        ON {$this->_aliases['civicrm_contact']}.id =
+                           {$this->_aliases['civicrm_email']}.contact_id AND
+                           {$this->_aliases['civicrm_email']}.is_primary = 1\n";
+    }
+  }
+
+  function where() {
+    $clauses = array();
+    foreach ($this->_columns as $tableName => $table) {
+      if (array_key_exists('filters', $table)) {
+        foreach ($table['filters'] as $fieldName => $field) {
+          $clause = NULL;
+          if (CRM_Utils_Array::value('operatorType', $field) & CRM_Utils_Type::T_DATE) {
+            $relative = CRM_Utils_Array::value("{$fieldName}_relative", $this->_params);
+            $from     = CRM_Utils_Array::value("{$fieldName}_from", $this->_params);
+            $to       = CRM_Utils_Array::value("{$fieldName}_to", $this->_params);
+
+            $clause = $this->dateClause($field['name'], $relative, $from, $to, $field['type']);
+          }
+          else {
+            $op = CRM_Utils_Array::value("{$fieldName}_op", $this->_params);
+            if ($op) {
+              $clause = $this->whereClause($field,
+                $op,
+                CRM_Utils_Array::value("{$fieldName}_value", $this->_params),
+                CRM_Utils_Array::value("{$fieldName}_min", $this->_params),
+                CRM_Utils_Array::value("{$fieldName}_max", $this->_params)
+              );
+            }
+          }
+
+          if (!empty($clause)) {
+            $clauses[] = $clause;
+          }
+        }
+      }
+    }
+
+    if (empty($clauses)) {
+      $this->_where = "WHERE ( 1 ) ";
+    }
+    else {
+      $this->_where = "WHERE " . implode(' AND ', $clauses);
+    }
+
+    if ($this->_aclWhere) {
+      $this->_where .= " AND {$this->_aclWhere} ";
+    }
+  }
+
+  function groupBy() {
+    $this->_groupBy = " GROUP BY {$this->_aliases['civicrm_contact']}.id, {$this->_aliases['civicrm_membership']}.membership_type_id";
+  }
+
+  function orderBy() {
+    $this->_orderBy = " ORDER BY {$this->_aliases['civicrm_contact']}.sort_name, {$this->_aliases['civicrm_contact']}.id, {$this->_aliases['civicrm_membership']}.membership_type_id";
+  }
+
+  function postProcess() {
+
+    $this->beginPostProcess();
+
+    $proposal_deets = ARLCRM\CustomGroup::getDetails( 'Proposal_Details');
+    $prop_deets_table = $proposal_deets['table_name'];
+    $prop_stage = $proposal_deets['fields']['Stage']['column_name'];
+    $prop_worth = $proposal_deets['fields']['Estimated Worth (£)']['column_name'];
+
+    $contract_deets = ARLCRM\CustomGroup::getDetails( 'Contract_Details');
+    $contract_deets_table = $contract_deets['table_name'];
+    $contract_worth = $contract_deets['fields']['Total Worth (£)']['column_name'];
+
+    // get activity type ids...
+    $activity_proposal = ARLCRM\OptionGroup::getValueValue('activity_type','Grant Funding Proposal');
+    $activity_contract = ARLCRM\OptionGroup::getValueValue('activity_type','Grant Funding Contract');
+    $activity_git      = ARLCRM\OptionGroup::getValueValue('activity_type','Get in touch');
+    $status_scheduled  = ARLCRM\OptionGroup::getValueValue('activity_status','Scheduled');
+    $status_live       = ARLCRM\OptionGroup::getValueValue('activity_status','Live');
+
+    // note1: your query
+    $sql = 
+        "SELECT 
+        IF(funder.id, CONCAT_WS('~',funder.id,funder.display_name), 'Missing Funder!') funder, 
+        CONCAT_WS('~',a.id, IF(a.subject!='', a.subject, 'Missing Project Name') ) title,
+        if( a.activity_type_id = $activity_contract,
+            FORMAT($contract_worth,0),
+            FORMAT($prop_worth,0)) worth,
+        IF( a.activity_type_id = $activity_contract,
+                'Contract',
+                prop_deets.$prop_stage ) stage,
+        CONCAT_WS('~',assignee.id,assignee.display_name) assigned,
+        ( SELECT git.id 
+          FROM civicrm_activity git
+          WHERE git.activity_type_id = $activity_git
+          AND git.status_id = $status_scheduled
+          AND git.parent_id = a.id
+          ORDER BY git.activity_date_time
+          LIMIT 1 ) `next`
+
+        FROM civicrm_activity a
+            LEFT JOIN civicrm_activity_contact cat ON (cat.activity_id = a.id AND cat.record_type_id = 3)
+            LEFT JOIN civicrm_contact funder ON cat.contact_id = funder.id
+            LEFT JOIN civicrm_activity_contact caa ON (caa.activity_id = a.id AND caa.record_type_id = 1)
+            LEFT JOIN civicrm_contact assignee ON caa.contact_id = assignee.id
+            LEFT JOIN $prop_deets_table prop_deets ON prop_deets.entity_id = a.id
+            LEFT JOIN $contract_deets_table contract_deets ON contract_deets.entity_id = a.id
+
+            WHERE
+            ( 
+             (  a.activity_type_id = $activity_proposal 
+                AND prop_deets.$prop_stage IN ('speculative','writing','waiting','negotiating') )
+             OR
+             (  a.activity_type_id = $activity_contract
+                AND a.status_id IN($status_scheduled, $status_live) ) 
+            )
+            ORDER BY funder.display_name";
+
+    /*
+       Add in assigned to (Staff)
+
+       Add in next action:
+       SELECT date, assigned to
+       FROM activity
+       WHERE activity_type = GIT
+       AND status='Scheduled'
+       AND parent_id = activity.id // only follow-ups 
+       ORDER BY date
+       LIMIT 1
+
+     */
+
+    // note2: register columns you want displayed-
+    $this->_columnHeaders =
+        array( 'funder' => array( 'title' => 'Funder' ),
+                'worth'  => array( 'title' => 'Worth £' ),
+                'title'  => array( 'title' => 'Project' ),
+                'stage' => array( 'title' => 'Stage' ),
+                'assigned'  => array('title' => 'Assigned to' ),
+                'next'  => array('title' => 'Next action' ),
+             );
+    // note3: let report do the fetching of records for you
+    $rows = array();
+    $this->buildRows($sql, $rows);
+
+    $this->formatDisplay($rows);
+    $this->doTemplateAssignment($rows);
+    $this->endPostProcess($rows);
+  }
+
+  function alterDisplay(&$rows) {
+    // custom code to alter rows
+    $entryFound = FALSE;
+    $checkList = array();
+    foreach ($rows as $rowNum => $row) {
+
+      if (!empty($this->_noRepeats) && $this->_outputMode != 'csv') {
+        // not repeat contact display names if it matches with the one
+        // in previous row
+        $repeatFound = FALSE;
+        foreach ($row as $colName => $colVal) {
+          if (CRM_Utils_Array::value($colName, $checkList) &&
+            is_array($checkList[$colName]) &&
+            in_array($colVal, $checkList[$colName])
+          ) {
+            $rows[$rowNum][$colName] = "";
+            $repeatFound = TRUE;
+          }
+          if (in_array($colName, $this->_noRepeats)) {
+            $checkList[$colName][] = $colVal;
+          }
+        }
+      }
+
+      if (array_key_exists('civicrm_membership_membership_type_id', $row)) {
+        if ($value = $row['civicrm_membership_membership_type_id']) {
+          $rows[$rowNum]['civicrm_membership_membership_type_id'] = CRM_Member_PseudoConstant::membershipType($value, FALSE);
+        }
+        $entryFound = TRUE;
+      }
+
+      if (array_key_exists('civicrm_address_state_province_id', $row)) {
+        if ($value = $row['civicrm_address_state_province_id']) {
+          $rows[$rowNum]['civicrm_address_state_province_id'] = CRM_Core_PseudoConstant::stateProvince($value, FALSE);
+        }
+        $entryFound = TRUE;
+      }
+
+      if (array_key_exists('civicrm_address_country_id', $row)) {
+        if ($value = $row['civicrm_address_country_id']) {
+          $rows[$rowNum]['civicrm_address_country_id'] = CRM_Core_PseudoConstant::country($value, FALSE);
+        }
+        $entryFound = TRUE;
+      }
+
+      if (array_key_exists('civicrm_contact_sort_name', $row) &&
+        $rows[$rowNum]['civicrm_contact_sort_name'] &&
+        array_key_exists('civicrm_contact_id', $row)
+      ) {
+        $url = CRM_Utils_System::url("civicrm/contact/view",
+          'reset=1&cid=' . $row['civicrm_contact_id'],
+          $this->_absoluteUrl
+        );
+        $rows[$rowNum]['civicrm_contact_sort_name_link'] = $url;
+        $rows[$rowNum]['civicrm_contact_sort_name_hover'] = ts("View Contact Summary for this Contact.");
+        $entryFound = TRUE;
+      }
+
+      if (!$entryFound) {
+        break;
+      }
+    }
+  }/*}}}*/
+
+
+    /** add in links etc. to results
+     */
+    public function formatDisplay(&$rows, $pager = TRUE)
+    {
+        foreach ($rows as &$row) {
+            // assignee
+            $row['assigned'] = preg_replace(
+                    '/^(\d+)~(.+)$/',
+                    '<a href="/civicrm/contact/view/?reset=1&amp;cid=$1" >$2</a>',
+                    $row['assigned']);
+
+            // funder
+            $funder_id = preg_replace( '/^(\d+)~.*$/','$1', $row['funder']);
+            $row['funder'] = preg_replace(
+                    '/^(\d+)~(.+)$/',
+                    '<a href="/civicrm/contact/view/?reset=1&amp;cid=$1" >$2</a>',
+                    $row['funder']);
+
+            // activity
+            $row['title'] = preg_replace(
+                    '/^(\d+)~(.+)$/',
+                    '<a href="/civicrm/activity/?action=view&amp;reset=1&amp;id=$1&amp;context=activity&amp;cid=' .$funder_id. '" >$2</a>',
+                    $row['title']);
+
+            // next activity
+            if (!($next_thing = (int) $row['next'])) {
+                continue;
+            }
+
+            // look up this activity. Going to need Assignee(s), date and subject
+            $sql = "SELECT a.id activity_id, subject, 
+                        a.activity_date_time datetime,
+                        assignee.id assignee_id, assignee.display_name assignee_name
+                FROM civicrm_activity a
+                     LEFT JOIN civicrm_activity_contact caa ON (caa.activity_id = a.id AND caa.record_type_id = 1)
+                     LEFT JOIN civicrm_contact assignee ON caa.contact_id = assignee.id
+                         WHERE a.id = $next_thing
+                         ";
+
+            $dao = CRM_Core_DAO::executeQuery($sql);
+            if (! $dao->fetch() ) {
+                unset($dao);
+                continue;
+            }
+
+            $due = strtotime($dao->datetime);
+            if (!$due) {
+                $due = '';
+            } else {
+                $interval = $due - time();
+                if ($interval < 0) {
+                    $interval = format_interval(-$interval) . " ago";
+                } else {
+                    $interval = "in " . format_interval($interval) ;
+                }
+                $due = date('j M Y', $due) . " ($interval)";
+            }
+            $row['next'] = 
+                "<a href='/civicrm/activity/?action=view&amp;reset=1&amp;id=$dao->activity_id&amp;context=activity&amp;cid=$funder_id' >"
+                . htmlspecialchars("$due $dao->subject")
+                . ($dao->assignee_name
+                        ? htmlspecialchars(" ($dao->assignee_name)")
+                        : '' )
+                . '</a>';
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,67 @@
+## Grant Funding, Tenders; Tracking Prospects and Contracts
+
+This can help you track your funding efforts and assist with making sure deadlines are met.
+
+*   A **Report** shows all live prospects/contracts and the next action that is due.
+*   Going to a (potential) funder's **Activities** tab will show you exactly where you're at with it.
+*   Going to your own **Activities report** will show you what you're responsible for!
+
+## How to use it
+
+It does this using three activity types:
+
+1.  **Prospect** this is used to record information on a grant proposal, tender etc. It has a **stage** field that tracks the prospect from initial idea through to successful/unsuccessful finish.
+2.  **Contract** this is used for successful prospects.
+3.  **Get in touch** this is used for **follow-up** activities to either a Prospect or a Contract.
+
+## Prospect activities
+
+This activity has the following **stages**:
+
+*  **Speculative / Seeking invitation**.
+
+*  **Writing proposal / tender.** Use this when you have made the decision to
+apply, and there is nothing stopping the work going ahead. Schedule a follow-up
+Get In Touch activity to record submission deadlines etc.
+
+*  **Awaiting Result.** The proposal/tender has been submitted and you're now
+just waiting. Schedule follow-up Get In Touch activity for the date you should
+hear by, then you can chase them after that.
+
+*  **Successful** Hooray. You got funded! You should create a **Contract**
+activity at this stage.
+
+*  **Unsuccessful**. Gah! Better luck next time.
+
+*  **Dropped by us**. For whatever reason you chose to stop the process.
+Record details in the Details field.
+
+*  **Negotiating**. Sometimes things aren't as simple and you need a holding
+or in-between state. This is here for those times.
+
+Note: You should leave a prospect as status: **Scheduled** until it completes
+(un/successful or dropped), at which point mark it **Completed**.
+
+## Contract activity
+
+Add one of these activity on successful completion of the prospect. Set the
+**status** to **Live** until the entire contract is completed, at which point
+update it to **Completed.**
+
+Add in any details required. Use the subject for the name of the project again. Assign the contract to a member of staff.
+
+Then schedule as many Get In Touch follow up activities as are needed to record significant timelines, for example:
+
+*   Milestone such-and-such to be reached.
+*   Year 1 report due from us
+*   Funding tranche 1 due from them
+*   Year 2 report due from us
+*   Funding tranche 2 due from them
+*   End of project report due.
+
+## Get in touch activities
+
+*   Always add these as **follow-up** activities from either a prospect or a contract.
+*   You can assign each of these to the appropriate member(s) of staff.
+*   Update them to status **Completed** when done.
+*   Use the Details field to record anything specific, but it might be helpful to keep most of the key information on either the prospect or the contract activity.
